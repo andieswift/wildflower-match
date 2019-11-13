2abeb2422d7c238ba6cc32d419bf571d3112e45e
Andie Swift <andie.swift5@gmail.com>
2019-11-12 17:02:00 -0800

    -did the HTML structure -added css to make the cards ands stats screen apear correctly -added JS to make each card 'flip' when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
+$(document).ready(intializeApp);
 
+function intializeApp(){
+  $(".card").on('click', handleCardClick);
+}
+
+function handleCardClick(event){
+    var target = $(event.currentTarget);
+    var front = target.find(".front");
+    var back = target.find(".back");
+    console.log(front);
+    front.removeClass("hide");
+    back.addClass("hide");
+}
