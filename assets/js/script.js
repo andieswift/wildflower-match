02ee6633ddb9ed39a3e7533869b61ea49d11e6e7
Andie Swift <andie.swift5@gmail.com>
2019-11-15 10:05:58 -0800

    stated practice 04

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,32 @@ $(document).ready(intializeApp);
 var firstCardClicked = null;
 var secondCardClicked = null;
 var matches = null;
-var max_matches = 2;
+var max_matches = 9;
+var attemps = null;
+var games_played = 0;
+var lockGame = false;
+var classArray = ["css-img", "css-img", "dock-img", "dock-img", "github-img", "github-img", "html-img", "html-img", "js-img",
+                  "js-img", "mysql-img", "mysql-img", "node-img", "node-img", "php-img", "php-img",
+                 "react-img", "react-img"];
 
 function intializeApp(){
-  $(".card").on('click', handleCardClick);
+
+  createCard();
+  $(".container").on('click','.card', handleCardClick);
   $("#close_btn").on('click', closeModal);
   $("#reset_btn").on('click', resetGame);
 }
 
 //stores card clicked and checks if they match
 function handleCardClick(event){
+  if(lockGame){
+    return;
+  }
+  var target = $(event.currentTarget);
+
+  if(target.find(".back").hasClass('hide')){
+    return;
+  }
     var target = $(event.currentTarget);
     var front = target.find(".front");
     var back = target.find(".back");
@@ -22,10 +38,11 @@ function handleCardClick(event){
 
     if(!firstCardClicked){
       firstCardClicked = target;
+
     }
     else{
       secondCardClicked = target;
-
+      attemps++;
       //checking if the cards match
       var firstFront = firstCardClicked.find(".front");
       var secondFront = secondCardClicked.find(".front");
@@ -37,18 +54,19 @@ function handleCardClick(event){
             secondCardClicked = null;
             //checks if they got all the matches
            if(matches === max_matches){
-                  console.log('YOU WIN');
-                  $(".modal").removeClass("hide")
+                  $(".modal").removeClass("hide");
+                  games_played++;
                 }
 
           }
           else{
+            lockGame = true;
              setTimeout(hideFrontCard, 1500);
 
           }
 
 
-
+        displayStats();
     }
 
   function hideFrontCard() {
@@ -60,8 +78,7 @@ function handleCardClick(event){
     secondFront.addClass("hide");
     firstCardClicked = null;
     secondCardClicked = null;
-
-
+    lockGame = false;
   }
 
 
@@ -80,4 +97,62 @@ function resetGame(){
   back.removeClass("hide");
   front.addClass("hide");
   matches = 0;
+  attemps = 0;
+  $("#Accuracy").text("0%");
+  $("#attemps").text(attemps);
+  $(".container").empty();
+  createCard();
+
 }
+
+function calculateAccuracy(){
+  var accuracy = matches/attemps;
+  accuracy = Math.round(10000 * accuracy) / 100;
+  var accuracyStr = accuracy + '%';
+  return accuracyStr;
+}
+
+function displayStats(){
+  var accuracy = calculateAccuracy();
+  $("#games_played").text(games_played);
+  $("#attemps").text(attemps);
+  $("#Accuracy").text(accuracy);
+}
+
+//Dynamically create cards
+function createCard(){
+  var arr = shuffle(classArray);
+  var container = $(".container");
+  for(var index=0; index< arr.length; index++){
+    var currentCard = $("<div>").addClass("card");
+    var classCard = arr[index];
+    var frontCard = $("<div>").addClass("front hide " + classCard);
+    var backCard = $("<div>").addClass("back");
+
+    container.append(currentCard);
+    currentCard.append(frontCard);
+    currentCard.append(backCard);
+  }
+}
+
+ function shuffle(array) {
+
+  var currentIndex = array.length;
+  var temporaryValue;
+  var randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+
+};
